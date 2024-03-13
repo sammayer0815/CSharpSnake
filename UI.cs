4659f3d1235b3e02f80a6f37862605c5b843a650
DPCoderr <s1173932@student.windesheim.nl>
2024-03-13 19:31:03 +0100

    ui main menu

--- UI.cs
+++ UI.cs
@@ -4,26 +4,30 @@ class UI
 {
     public void MainMenu()
     {
-        Console.WriteLine("┏━━━━━━━━━━━━━━━━━┓");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃      SNAKE      ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃      Play       ┃");
-        Console.WriteLine("┃      HiScore    ┃");
-        Console.WriteLine("┃      Settings   ┃");
-        Console.WriteLine("┃      Quit       ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃     Made By     ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃      Sam        ┃");
-        Console.WriteLine("┃      Sadek      ┃");
-        Console.WriteLine("┃      Daniel     ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┗━━━━━━━━━━━━━━━━━┛");
+        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃      _________              __                ┃");
+        Console.WriteLine("┃     /   _____/ ____ _____  |  | __ ____       ┃");
+        Console.WriteLine("┃     \\_____  \\ /    \\\\__  \\ |  |/ // __ \\      ┃");
+        Console.WriteLine("┃     /        \\   |  \\/ __ \\|    <\\  ___/      ┃");
+        Console.WriteLine("┃    /_______  /___|  (____  /__|_ \\\\___  >     ┃");
+        Console.WriteLine("┃            \\/     \\/     \\/     \\/    \\/      ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                    Play                       ┃");
+        Console.WriteLine("┃                    Highscore                  ┃");
+        Console.WriteLine("┃                    Settings                   ┃");
+        Console.WriteLine("┃                    Quit                       ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                    Made by                    ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                    Sam                        ┃");
+        Console.WriteLine("┃                    Sadek                      ┃");
+        Console.WriteLine("┃                    Daniel                     ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
     }
 
     public void GameMenu()
