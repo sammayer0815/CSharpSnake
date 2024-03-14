034ad50a217d622ed756687d6bbd6effe57943de
DPCoderr <s1173932@student.windesheim.nl>
2024-03-14 01:17:24 +0100

    ui highscore menu

--- UI.cs
+++ UI.cs
@@ -60,26 +60,30 @@ class UI
 
     public void HighscoreMenu()
     {
-        Console.WriteLine("┏━━━━━━━━━━━━━━━━━┓");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃   Highscore:    ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┃                 ┃");
-        Console.WriteLine("┗━━━━━━━━━━━━━━━━━┛");
+        Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┃                   Highscore                   ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+        Console.WriteLine("┃                Difficulty: Easy               ┃");
+        Console.WriteLine("┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫");
+        Console.WriteLine("┃    1.                                         ┃");
+        Console.WriteLine("┃    2.                                         ┃");
+        Console.WriteLine("┃    3.                                         ┃");
+        Console.WriteLine("┃    4.                                         ┃");
+        Console.WriteLine("┃    5.                                         ┃");
+        Console.WriteLine("┃    6.                                         ┃");
+        Console.WriteLine("┃    7.                                         ┃");
+        Console.WriteLine("┃    8.                                         ┃");
+        Console.WriteLine("┃    9.                                         ┃");
+        Console.WriteLine("┃    10.                                        ┃");
+        Console.WriteLine("┃    11.                                        ┃");
+        Console.WriteLine("┃    12.                                        ┃");
+        Console.WriteLine("┃    13.                                        ┃");
+        Console.WriteLine("┃    14.                                        ┃");
+        Console.WriteLine("┃    15.                                        ┃");
+        Console.WriteLine("┃                                               ┃");
+        Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
     }
 
     public void SettingsMenu()
