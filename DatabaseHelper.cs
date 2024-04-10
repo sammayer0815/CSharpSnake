20164498f0f41d15edcd73b1cacd0f8d22c44883
Sadek <39184773+Sadek2002@users.noreply.github.com>
2024-04-10 14:59:28 +0200

    Database connection
    Made connection with the database

---
+++ DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+
+public static class DatabaseHelper
+{
+    private static string connectionString = @"Data Source-..\..\files\Snake.db;Version=3;";
+
+    public static void InitializeDatabase()
+    {
+        if (!File.Exists(@"..\..\files\Snake.db"))
+        {
+            SQLiteConnection.CreateFile(@"..\..\files\Snake.db");
+                using (var connection = new SQLiteConnection(connectionString))
+                {
+                connection.Open();
+
+                // Create tables for database
+                string createSnakeDataTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS SnakeData (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        username TEXT NOT NULL,
+                        score INTEGER NOT NULL,
+                        time INTEGER NOT NULL,
+                        difficulty TEXT NOT NULL
+                    );";
+
+                using (var command = new SQLiteCommand(connection)) 
+                {
+                    command.CommandText = createSnakeDataTableQuery;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main()
     {
+        DatabaseHelper.InitializeDatabase();
         Console.Clear();
         Console.OutputEncoding = System.Text.Encoding.UTF8;
         UI UIInstance = new UI();
